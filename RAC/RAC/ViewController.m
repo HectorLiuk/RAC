4214c26c29ea98832491d26f119c7c936d61735e
liukan <liukan@xueqiu.com>
2017-01-06 11:14:11 +0800

    add

--- RAC/RAC/ViewController.m
+++ RAC/RAC/ViewController.m
@@ -10,6 +10,11 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+
+@property (nonatomic, copy) NSDictionary *dic;
+@property (nonatomic, copy) NSString *str;
 @end
 
 @implementation ViewController
@@ -18,14 +23,27 @@
     [super viewDidLoad];
     
     [self creatSignal];
+    
+    [self rac];
+    
+
+    self.dic = @{@"ss":@"sss"};
+    
+    [self kvo];
+
+    
+//    [RACObserve(self, dic) subscribeNext:^(id x) {
+//        
+//    }];
+    
 
 }
 
 - (void)creatSignal{
     RACSignal *signal = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
-        [subscriber sendNext:@1];
+        [subscriber sendNext:self.textField.text];
         
-        [subscriber sendCompleted];
+//        [subscriber sendCompleted];
         
         
         return [RACDisposable disposableWithBlock:^{
@@ -35,10 +53,28 @@
     
     [signal subscribeNext:^(id x) {
         NSLog(@"接受信号%@",x);
+    }];
+}
+
+//8.1 RAC(TARGET, [KEYPATH, [NIL_VALUE]]):用于给某个对象的某个属性绑定。
+- (void)rac
+{
+    RAC(self.titleLabel,text) = self.textField.rac_textSignal;
+}
+
+- (void)kvo
+{
+    [[self rac_valuesAndChangesForKeyPath:@"dic" options:NSKeyValueObservingOptionNew observer:nil] subscribeNext:^(id x) {
         
     }];
 }
 
+- (void)ss
+{
+    //    [RACObserve(self, dic) subscribeNext:^(id x) {
+    //
+    //    }];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
